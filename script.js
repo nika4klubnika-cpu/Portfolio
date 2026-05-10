37bd0a92fe0b89892b6d6fa281e2e4de2065f0a0
nika4klubnika-cpu <nika4klubnika@gmail.com>
2026-05-10 10:56:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+    const modal = document.getElementById('video-modal');
+    const modalVideo = document.getElementById('modal-video');
+    const closeBtn = document.querySelector('.close-modal');
+    const projectItems = document.querySelectorAll('.project-item');
